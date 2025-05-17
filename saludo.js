c94a87d28ce81e34730b53344cc0bf3c5ec03d47
josecruz2511b <122044091@upq.edu.mx>
2025-05-16 18:49:52 -0600

    3 ejercico
    Saludo

--- saludo.js
+++ saludo.js
@@ -1,13 +1,3 @@
-let nombre = "Armando";
-const edad = 25;
-
-nombre = "Juan";
-
-const saludo = `Hola ${nombre}, tienes ${edad} años.`;
-console.log(saludo);
-
-
-
 const saludoPersonalizado = (nombre, edad) => { 
     return `Hola ${nombre}, tienes ${edad} años.`;
 }
