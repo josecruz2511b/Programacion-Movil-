d46f9ee7ebd9db9d600ae099cd7051864a1e48b3
josecruz2511b <122044091@upq.edu.mx>
2025-05-19 21:21:18 -0600

    ejercicio 2
    filtrar y map

---
+++ Practica2/ejercicio2.js
@@ -0,0 +1,11 @@
+const productos = [
+    { nombre: "Laptop", precio: 12000 },
+    { nombre: "Mouse", precio: 250 },
+    { nombre: "Teclado", precio: 750 },
+    { nombre: "Monitor", precio: 3000 }
+];
+
+//tu codigo aqui
+const nombres = productos.filter(producto => producto.precio > 1000)
+.map(producto => producto.nombre);
+console.log(nombres);
