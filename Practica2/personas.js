1657a8b1203a20fd0a3baee6999f2eba3522c966
josecruz2511b <122044091@upq.edu.mx>
2025-05-19 22:13:48 -0600

    a
    a

--- Practica2/personas.js
+++ Practica2/personas.js
@@ -3,13 +3,14 @@ const personas = [
     {nombre: "Luis", edad: 35},
     {nombre: "Maria", edad: 28}
 ];
-
+// Filtrar el nombre de Luis 
 const personaBuscada = personas.find(personas =>personas.nombre === "Luis");
 console.log(personaBuscada);
-
+// Filtrar el nombre y edad de cada persona
 personas.forEach((persona, index) => {
     console.log ('Nombre:', persona.nombre, 'Edad:', persona.edad)});
-
+// Sumar las edades de todas las personas
+// Usando reduce
 const sumaEdades = personas.reduce((acumulador ,persona ) => {
     return acumulador + persona.edad;
 }, 0);
