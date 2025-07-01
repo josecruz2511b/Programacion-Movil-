d76db160fd3e631cb87bc314727895c0a7cdc45f
josecruz2511b <122044091@upq.edu.mx>
2025-06-30 23:29:13 -0600

    a

--- Blank2/index.js
+++ Blank2/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './practicarepaso';
+import App from './scrollView';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
