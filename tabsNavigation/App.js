a5ae322ea2080b6449903f73b1ac7ae30c01e449
josecruz2511b <122044091@upq.edu.mx>
2025-07-18 18:20:19 -0600

    Tabs & Stack
    Tabs & Stack

--- tabsNavigation/App.js
+++ tabsNavigation/App.js
@@ -1,44 +1,54 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {Ionicons} from '@expo/vector-icons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Home from './screens/home';
-import Profile from './screens/profile';
 import Settings from './screens/settings';
+import Profile from './screens/profile';
+import UserDetail from './screens/userDetail'; 
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function StackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
+      <Stack.Screen name="UserDetail" component={UserDetail} options={{ title: 'Detalles de Usuario' }} />
+    </Stack.Navigator>
+  );
+}
+
 export default function App() {
   return (
-  <NavigationContainer>
-    <Tab.Navigator
-    initialRouteName='Home'
-    screenOptions={({ route }) => ({
-      headerShown: false,
-      tabBarIcon: ({ color, size }) => {
-        let iconName;
-
-        if (route.name === 'Home') {
-          iconName = 'home';
-        } else if (route.name === 'Profile') {
-          iconName = 'person';
-        } else if (route.name === 'Settings') {
-          iconName = 'settings';
-        }
-
-        return <Ionicons name={iconName} size={size} color={color} />;
-      },
-      tabBarActiveTintColor: '#007BFF',
-      tabBarInactiveTintColor: 'gray',
-      tabBarStyle: {
-        paddingBottom: 5,
-        height: 60,
-      },
-    })}
-    >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Settings" component={Settings} />
-    </Tab.Navigator>
-  </NavigationContainer>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Profile') {
+              iconName = 'person';
+            } else if (route.name === 'Settings') {
+              iconName = 'settings';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#01080eff',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false,
+          tabBarStyle: {
+            paddingBottom: 5,
+            height: 60,
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Profile" component={StackScreen} options={{ title: 'Profile' }} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
--- tabsNavigation/screens/profile.js
+++ tabsNavigation/screens/profile.js
@@ -1,12 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Profile() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <View style={styles.iconRow}>
         <Ionicons name="person-outline" size={28} color="green" />
-        <Text style={styles.title}> Perfil de usuario </Text>
+        <Text style={styles.title}>Perfil de usuario</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Ver Detalles"
+          onPress={() => navigation.navigate('UserDetail')}
+        />
       </View>
     </View>
   );
@@ -28,6 +37,44 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: 'bold',
     marginLeft: 10,
-    color: 'green',
+    color: '#000000ff', 
+    marginBottom: 20,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+  },
+  buttonContainer: {
+    marginTop: 20,
+    width: '80%',
+    backgroundColor: '#e0f23bff',
+    borderColor: '#06080bff',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#000000ff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+    marginBottom: 20,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonProfile: {
+    backgroundColor: '#010101ff', // Azul
+  },
+  buttonSettings: {
+    backgroundColor: '#FF8800', // Naranja
+  },
+  buttonText: {
+    color: '#010101ff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  buttonHome: {
+    backgroundColor: '#28A745', // Verde
   },
 });
